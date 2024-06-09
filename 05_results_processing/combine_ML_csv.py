ac61f0257d0e1767fc9e3fd449e640b4fc0f45a2
peppermint-ice <d.usenko@hotmail.com>
2024-06-09 09:29:11 +0300

    initial commit 0302getstat is already good

--- 05_results_processing/combine_ML_csv.py
+++ 05_results_processing/combine_ML_csv.py
@@ -6,8 +6,8 @@ import pandas as pd
 folder_paths = paths.get_paths()
 
 # Set folder
-csv_folder_path = folder_paths["ml_results"]
-csv_export_path = os.path.join(folder_paths["data"], "combine_ML7_kf_no_outliers.csv")
+csv_folder_path = folder_paths["results"]
+csv_export_path = os.path.join(folder_paths["combined"], "combine_ML8_kf.csv")
 csvs = os.listdir(csv_folder_path)
 
 df = pd.DataFrame()
@@ -16,10 +16,7 @@ for file in csvs:
     file_path = os.path.join(csv_folder_path, file)
     if os.path.isfile(file_path) and file_path.lower().endswith('.csv'):
         df_current = pd.read_csv(file_path)
-        # if 'noyear' in file:
-        #     df_current.loc[:, 'by_year'] = False
-        # else:
-        #     df_current.loc[:, 'by_year'] = True
+
         df = pd.concat([df, df_current], ignore_index=True)
 
 df.to_csv(csv_export_path, index=False)
