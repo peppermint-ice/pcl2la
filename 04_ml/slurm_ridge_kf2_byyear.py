ae2bdb154e9f7380f5d8a4e3adf977f664ebebe8
peppermint-ice <d.usenko@hotmail.com>
2024-07-24 08:53:00 +0300

    by_year

--- 04_ml/slurm_ridge_kf2_byyear.py
+++ 04_ml/slurm_ridge_kf2_byyear.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
         X_test_boruta = X_test_mi[selected_features_boruta]
 
         # Save the global test set
-        global_test_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_ridge_global_test_set.csv"
+        global_test_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_ridge_global_test_set_byyear.csv"
         global_test_filepath = os.path.join(global_test_path, global_test_filename)
         test_selected = pd.concat([X_test_boruta, y_test.reset_index(drop=True)], axis=1)
         test_selected.to_csv(global_test_filepath, index=False)
@@ -151,13 +151,13 @@ if __name__ == '__main__':
             }
             results = pd.concat([results, pd.DataFrame([current_results])], ignore_index=True)
             # Save train and validation datasets as CSV files
-            train_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_train_kf_ridge_fold_{i}.csv"
+            train_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_train_kf_ridge_fold_{i}_byyear.csv"
             train_filepath = os.path.join(train_folder_path, train_filename)
             train_set = X_train_fold.copy()
             train_set['measured_leaf_area'] = y_train_fold
             train_set.to_csv(train_filepath, index=False)
 
-            val_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_val_kf_ridge_fold_{i}.csv"
+            val_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_val_kf_ridge_fold_{i}_byyear.csv"
             val_filepath = os.path.join(test_folder_path, val_filename)
             val_set = X_val_fold.copy()
             val_set['measured_leaf_area'] = y_val_fold
@@ -170,7 +170,7 @@ if __name__ == '__main__':
                 best_model = model
 
         # Save the best model using pickle
-        model_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_best_model_ridge.pkl"
+        model_filename = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_best_model_ridge_byyear.pkl"
         model_filepath = os.path.join(model_folder_path, model_filename)
         with open(model_filepath, 'wb') as f:
             pickle.dump(best_model, f)
@@ -192,12 +192,12 @@ if __name__ == '__main__':
             'R2_score_test': r2_test
         }
         test_results_df = pd.DataFrame([test_results])
-        test_output_file = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_test_results_ridge.csv"
+        test_output_file = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_test_results_ridge_byyear.csv"
         test_output_file_path = os.path.join(csv_folder_path, test_output_file)
         test_results_df.to_csv(test_output_file_path, index=False)
 
         # Save the k-fold results
-        kfold_output_file = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_kfold_results_ridge.csv"
+        kfold_output_file = f"{parameter_name}_{parameter_value}_{assessment_name}_{repaired}_{eliminated}_kfold_results_ridge_byyear.csv"
         kfold_output_file_path = os.path.join(kfold_results_path, kfold_output_file)
         results.to_csv(kfold_output_file_path, index=False)
 
--- 06_all_models_evaluation/06_03_save_all_models_results.py
+++ 06_all_models_evaluation/06_03_save_all_models_results.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score, mean_squared_error
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 from config import paths
 from datetime import datetime
 
@@ -24,29 +25,23 @@ def plot_prediction(y_pred, y, model_parameters, subset_name):
     r2 = r2_score(y, y_pred)
     if r2 > 0.85:
         plt.scatter(y, y_pred)
-        plt.title(f"R2 score for {model_parameters[0]} {model_parameters[1]} {model_parameters[5]} {subset_name}: {round(r2, 2)}")
+        plt.title(
+            f"R2 score for {model_parameters[0]} {model_parameters[1]} {model_parameters[5]} {subset_name}: {round(r2, 2)}")
         plt.show()
 
 
 if __name__ == '__main__':
-    # Set random seed for reproducibility
-    # set_random_seed()
-
     # Set folder paths
     folder_paths = paths.get_paths()
     models_folder_path = folder_paths["models"]
     global_test_sets_path = folder_paths["global_test_sets"]
     train_folder_path = folder_paths["train_sets"]
     test_folder_path = folder_paths["test_sets"]
-    ready_for_training_path = folder_paths["ready_for_training"]
     scalers_folder_path = folder_paths["scalers"]
+    ready_for_training_path = folder_paths["ready_for_training"]
 
     # List to store results
     results = []
-
-    # Initiate dfs for R2s and worst points
-    worst_points = pd.DataFrame()
-    worst_points_current = pd.DataFrame()
 
     # Run through each model file
     models = os.listdir(models_folder_path)
@@ -67,113 +62,116 @@ if __name__ == '__main__':
                 byyear = match.group(7)
                 file_type = match.group(8)
                 # Save them into one list
-                model_parameters = [algorithm_name, parameter_value, assessment_name, dataset_type, elimination_status, regression_model, byyear]
+                model_parameters = [algorithm_name, parameter_value, assessment_name, dataset_type, elimination_status,
+                                    regression_model, byyear]
                 print("File:")
-                print(algorithm_name, parameter_value, assessment_name, dataset_type, elimination_status, regression_model, byyear)
-                # We are only interested in noElim files
-                if elimination_status == "noElim":
-                    # Create file names
-                    # Create test and train set lists
-                    test_file_names = []
-                    train_file_names = []
-                    for i in range(6):
-                        test_file_names.append(f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_val_kf_{regression_model}_fold_{i}.csv")
-                        train_file_names.append(f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_train_kf_{regression_model}_fold_{i}.csv")
-                    # Define global test set file name
-                    global_test_set_file_name = f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_{regression_model}_global_test_set.csv"
-                    # Define files paths
-                    # Model file path is different for rf and xgb because the XGBoost needs to load a json
-                    if regression_model == "xgb":
-                        json_file_name = f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_best_model_{regression_model}.json"
-                        model_file_path = os.path.join(models_folder_path, json_file_name)
+                print(algorithm_name, parameter_value, assessment_name, dataset_type, elimination_status,
+                      regression_model, byyear)
+                # Create file names
+                test_file_names = []
+                train_file_names = []
+                for i in range(6):
+                    test_file_names.append(
+                        f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_val_kf_{regression_model}_fold_{i}.csv")
+                    train_file_names.append(
+                        f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_train_kf_{regression_model}_fold_{i}.csv")
+                # Define global test set file name
+                global_test_set_file_name = f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_{regression_model}_global_test_set.csv"
+                # Define files paths
+                if regression_model == "xgb":
+                    json_file_name = f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_best_model_{regression_model}.json"
+                    model_file_path = os.path.join(models_folder_path, json_file_name)
+                else:
+                    model_file_path = os.path.join(models_folder_path, model)
+                global_test_set_file_path = os.path.join(global_test_sets_path, global_test_set_file_name)
+                test_file_paths = [os.path.join(test_folder_path, test_file_names[i]) for i in
+                                   range(len(test_file_names))]
+                train_file_paths = [os.path.join(train_folder_path, train_file_names[i]) for i in
+                                    range(len(train_file_names))]
+
+                # Load model
+                with open(model_file_path, 'rb') as file:
+                    if regression_model == 'xgb':
+                        model = xgb.Booster()
+                        model.load_model(model_file_path)
                     else:
-                        model_file_path = os.path.join(models_folder_path, model)
-                    global_test_set_file_path = os.path.join(global_test_sets_path, global_test_set_file_name)
-                    test_file_paths = [os.path.join(test_folder_path, test_file_names[i]) for i in range(len(test_file_names))]
-                    train_file_paths = [os.path.join(train_folder_path, train_file_names[i]) for i in range(len(train_file_names))]
+                        model = pickle.load(file)
+                print("Model loaded")
 
-                    # Load files
-                    # Load models
-                    with open(model_file_path, 'rb') as file:
-                        if regression_model == 'xgb':
-                            model = xgb.Booster()
-                            model.load_model(model_file_path)
-                        else:
-                            model = pickle.load(file)
-                    print("Model loaded")
-                    # Load test set
-                    global_test_df = pd.read_csv(global_test_set_file_path)
-                    # Load test and train datasets
-                    test_dfs = [pd.read_csv(test_file_paths[i]) for i in range(len(test_file_paths))]
-                    train_dfs = [pd.read_csv(train_file_paths[i]) for i in range(len(train_file_paths))]
-                    print("Files loaded")
-                    print(train_file_paths)
+                # Load test set
+                global_test_df = pd.read_csv(global_test_set_file_path)
+                # Load test and train datasets
+                test_dfs = [pd.read_csv(test_file_paths[i]) for i in range(len(test_file_paths))]
+                train_dfs = [pd.read_csv(train_file_paths[i]) for i in range(len(train_file_paths))]
+                print("Files loaded")
 
-                    # # Get lengths of test and train sets
-                    # test_set_length = len(global_test_df)
-                    # for train_set in train_dfs:
-                    #     all_data = pd.concat([train_set], ignore_index=True)
-                    #
-                    # train_set_length += len(train_set)
+                # Load scaler if elimination status is "elim"
+                if elimination_status == "elim":
+                    scaler_file_path = os.path.join(scalers_folder_path,
+                                                    f"{algorithm_name}_{parameter_value}_{assessment_name}_{dataset_type}_{elimination_status}_scaler_{regression_model}_{byyear}.pickle")
+                    with open(scaler_file_path, 'rb') as scaler_file:
+                        scaler = pickle.load(scaler_file)
+                    print("Scaler loaded")
 
-                    # GLOBAL TEST
-                    print("Global test")
-                    # Extract X and Y
-                    x = global_test_df.drop('measured_leaf_area', axis=1)
-                    y = global_test_df['measured_leaf_area']
+                # GLOBAL TEST
+                print("Global test")
+                # Extract X and Y
+                x = global_test_df.drop('measured_leaf_area', axis=1)
+                y = global_test_df['measured_leaf_area']
+                # Scale X if elimination status is "elim"
+                if elimination_status == "elim":
+                    x = scaler.transform(x)
+                # Run the model
+                y_pred = run_any_model(regression_model, x, model)
+                # Get R2 and RMSE
+                r2_global_test = r2_score(y, y_pred)
+                rmse_global_test = mean_squared_error(y, y_pred, squared=False)
+                print("R2 score: ", r2_global_test)
+
+                # TESTING SETS
+                print("Testing sets")
+                r2_testing = []
+                for test_df in test_dfs:
+                    x = test_df.drop('measured_leaf_area', axis=1)
+                    y = test_df['measured_leaf_area']
+                    # Scale X if elimination status is "elim"
+                    if elimination_status == "elim":
+                        x = scaler.transform(x)
                     # Run the model
                     y_pred = run_any_model(regression_model, x, model)
                     # Get R2
-                    r2_global_test = r2_score(y, y_pred)
-                    rmse_global_test = mean_squared_error(y, y_pred, squared=False)
-                    print("R2 score: ", r2_global_test)
-                    # plot_prediction(y, y_pred, model_parameters, 'GLOBAL test subset')
+                    r2 = r2_score(y, y_pred)
+                    r2_testing.append(r2)
+                    print("R2 score: ", r2)
 
-                    # TESTING SETS
-                    print("Testing sets")
-                    # A loop to do the procedure at once for each kfold
-                    r2_testing = []
-                    for test_df in test_dfs:
-                        x = test_df.drop('measured_leaf_area', axis=1)
-                        y = test_df['measured_leaf_area']
-                        # Run the model
-                        y_pred = run_any_model(regression_model, x, model)
-                        # Get R2
-                        r2 = r2_score(y, y_pred)
-                        r2_testing.append(r2)
-                        print("R2 score: ", r2)
-                        # plot_prediction(y, y_pred, model_parameters, 'testing subset')
+                # TRAINING SETS
+                print("Training sets")
+                r2_training = []
+                for train_df in train_dfs:
+                    x = train_df.drop('measured_leaf_area', axis=1)
+                    y = train_df['measured_leaf_area']
+                    # Scale X if elimination status is "elim"
+                    if elimination_status == "elim":
+                        x = scaler.transform(x)
+                    # Run the model
+                    y_pred = run_any_model(regression_model, x, model)
+                    # Get R2
+                    r2 = r2_score(y, y_pred)
+                    r2_training.append(r2)
+                    print("R2 score: ", r2)
 
-                    # TRAINING SETS
-                    print("Training sets")
-                    # A loop to do the procedure at once for each kfold
-                    r2_training = []
-                    for train_df in train_dfs:
-                        x = train_df.drop('measured_leaf_area', axis=1)
-                        y = train_df['measured_leaf_area']
-                        # Run the model
-                        y_pred = run_any_model(regression_model, x, model)
-                        # Get R2
-                        r2 = r2_score(y, y_pred)
-                        r2_training.append(r2)
-                        print("R2 score: ", r2)
-                        # plot_prediction(y, y_pred, model_parameters, 'training subset')
+                # Calculate means for training and testing
+                mean_r2_training = np.mean(r2_training)
+                mean_r2_testing = np.mean(r2_testing)
 
-                    # Calculate means for training and testing
-                    mean_r2_training = np.mean(r2_training)
-                    mean_r2_testing = np.mean(r2_testing)
-
-                    # Save the results
-                    # Append results to the list
-                    result = (model_parameters + r2_training + r2_testing +
-                              [mean_r2_training, mean_r2_testing, r2_global_test, rmse_global_test])
-                    results.append(result)
-                    print("")
+                # Save the results
+                result = (model_parameters + r2_training + r2_testing +
+                          [mean_r2_training, mean_r2_testing, r2_global_test, rmse_global_test])
+                results.append(result)
+                print("")
 
     # Save results to CSV
-    # Get current time and format it
     current_time = datetime.now().strftime("%d%m%y_%H%M")
-    # Create a table
     columns = ([
                    "algorithm_name",
                    "parameter_value",
